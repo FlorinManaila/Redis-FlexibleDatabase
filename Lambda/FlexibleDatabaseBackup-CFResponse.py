14ca485dd4329d7fc2b05fac3dde5ead72c24c40
florin-nglz-at-191310524501 <florin.manaila@allcloud.io>
2023-10-30 18:42:42 +0200

    updated lambdas v1.0.0

--- Lambda/FlexibleDatabaseBackup-CFResponse.py
+++ Lambda/FlexibleDatabaseBackup-CFResponse.py
@@ -1,14 +1,13 @@
 import json
-import json
 import cfnresponse
 import requests
 
+#This lambda only send back to CloudFormation the response received from the previous function as displayed on the Step Functions state machine flow
 def lambda_handler(event, context):
     print (event)
     responseURL = event["responseURL"]
     responseBody = event["responseBody"]
     GetResponse(responseURL, responseBody)
-
 
 def GetResponse(responseURL, responseBody): 
     responseBody = json.dumps(responseBody)
